855052c8966d3eaab04375e8d973b16142ad8017
严文强 <fanmucc@yeah.net>
2023-05-13 23:26:46 +0800

    Feat: markdown样式合并

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -1,11 +1,9 @@
-import styles from "@/styles/markdown.module.scss";
-
 const Markdown = (props: { markdown: string }) => {
 	return (
-		<div id={styles["post"]}>
+		<div id='post'>
 			<div
-				className={styles["markdown-content"]}
-				id={styles["article-container"]}
+				className='markdown-content'
+				id='article-container'
 				dangerouslySetInnerHTML={{ __html: props?.markdown || "" }}
 			></div>
 		</div>
